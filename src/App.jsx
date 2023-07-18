791d4226708c2ff7e9c209f5be07405167c6b295
Jake Samuels <samuels242@googlemail.com>
2023-07-18 12:03:32 +0100

    Add routes for Login and Jobspage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header/Header";
+import Login from "./pages/Login";
+import AllJobs from "./pages/AllJobs";
 
 const App = () => {
   return (
     <main>
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/jobs" element={<AllJobs />} />
+      </Routes>
     </main>
   );
 };
