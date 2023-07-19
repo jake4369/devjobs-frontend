23af23e1e7381a7d556f7c117a08fc814a4a30f0
Jake Samuels <samuels242@googlemail.com>
2023-07-19 18:19:05 +0100

    Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,46 @@
+import { useState } from "react";
+
+import LoginForm from "../components/Login/LoginForm";
+import RegisterationForm from "../components/Login/RegisterationForm";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [loginDetails, setLoginDetails] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [registrationDetails, setRegistrationDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    role: "",
+  });
+
+  const [register, setRegister] = useState(false);
+
+  return (
+    <div className="login-page">
+      <h1>
+        {!register
+          ? "Log in to discover you dream dev job"
+          : "Register with devjobs"}
+      </h1>
+
+      {!register ? (
+        <LoginForm
+          loginDetails={loginDetails}
+          setLoginDetails={setLoginDetails}
+          setRegister={setRegister}
+        />
+      ) : (
+        <RegisterationForm
+          registrationDetails={registrationDetails}
+          setRegistrationDetails={setRegistrationDetails}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Login;
