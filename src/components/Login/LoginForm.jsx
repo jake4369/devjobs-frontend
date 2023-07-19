8060e21fa7746621ed4f9b3cbc9f23abdb908704
Jake Samuels <samuels242@googlemail.com>
2023-07-19 18:19:28 +0100

    Login and Registration form components

---
+++ src/components/Login/LoginForm.jsx
@@ -0,0 +1,74 @@
+import { login } from "./../../utils/api";
+
+const LoginForm = ({ loginDetails, setLoginDetails, setRegister }) => {
+  const handleChange = (e) => {
+    setLoginDetails((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Attempt to log in and get the token
+      const token = await login(loginDetails.email, loginDetails.password);
+
+      if (token) {
+        // Redirect to the specified URL
+        window.location.href = "http://localhost:5173/jobs";
+      } else {
+        // Handle login failure, show an error message, etc.
+        console.log("Login failed. Please check your credentials.");
+      }
+
+      setLoginDetails({
+        email: "",
+        password: "",
+      });
+    } catch (error) {
+      console.error("Login failed:", error);
+      // Handle login errors here or show a generic error message.
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="login-form">
+      <label>
+        Email:
+        <input
+          type="email"
+          placeholder="example@gmail.com"
+          name="email"
+          value={loginDetails.email}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={loginDetails.password}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button className="login-btn">Log In</button>
+
+      <span>
+        Don't have an account? Register{" "}
+        <span className="register-link" onClick={() => setRegister(true)}>
+          here
+        </span>
+      </span>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/Login/RegisterationForm.jsx
@@ -0,0 +1,116 @@
+import { signUp } from "./../../utils/api";
+
+const RegisterationForm = ({ registrationDetails, setRegistrationDetails }) => {
+  const handleChange = (e) => {
+    setRegistrationDetails((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Call the signUp function with the form data
+      const token = await signUp(
+        registrationDetails.name,
+        registrationDetails.email,
+        registrationDetails.password,
+        registrationDetails.passwordConfirm,
+        registrationDetails.role
+      );
+
+      if (token) {
+        // Redirect to the specified URL
+        window.location.href = "http://localhost:5173/jobs";
+      } else {
+        // Handle login failure, show an error message, etc.
+        console.log("Login failed. Please check your credentials.");
+      }
+      // Optionally, you can handle successful registration here (e.g., show a success message).
+      console.log("Successfully registered and logged in.");
+
+      // Clear the registration details after successful registration
+      setRegistrationDetails({
+        name: "",
+        email: "",
+        password: "",
+        passwordConfirm: "",
+        role: "",
+      });
+    } catch (error) {
+      // Handle any errors that occurred during registration (e.g., display an error message).
+      console.error("Registration failed:", error);
+    }
+  };
+
+  return (
+    <form className="registration-form" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          placeholder="Full name"
+          name="name"
+          value={registrationDetails.name}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          placeholder="example@gmail.com"
+          name="email"
+          value={registrationDetails.email}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={registrationDetails.password}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Confirm password:
+        <input
+          type="password"
+          placeholder="Password"
+          name="passwordConfirm"
+          value={registrationDetails.passwordConfirm}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Are you here to:
+        <select
+          name="role"
+          value={registrationDetails.role}
+          onChange={handleChange}
+        >
+          <option value="" disabled>
+            ---
+          </option>
+          <option value="user">Find a job</option>
+          <option value="employer">Hire employees</option>
+        </select>
+      </label>
+
+      <button className="register-btn">Register</button>
+    </form>
+  );
+};
+
+export default RegisterationForm;
