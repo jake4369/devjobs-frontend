0d151b3cacb1a87768af36c6b7698fd8fd2a7b80
Jake Samuels <samuels242@googlemail.com>
2023-07-19 10:28:47 +0100

    All Jobs page

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -1,11 +1,15 @@
 import FilterModal from "../components/AllJobsPage/FilterModal";
+import JobCard from "../components/AllJobsPage/JobCard";
 
-const AllJobs = ({ showModal, setShowModal }) => {
+const AllJobs = ({ showModal, setShowModal, jobsData }) => {
+  const jobCards = jobsData.map((job) => {
+    return <JobCard key={job.id} job={job} />;
+  });
+
   return (
     <div className="all-jobs-page">
-      <p>All Jobs</p>
-
       {showModal && <FilterModal setShowModal={setShowModal} />}
+      <section className="job-cards">{jobCards}</section>
     </div>
   );
 };
