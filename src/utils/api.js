e9c1379fa814b2846e9b1d0bab486f9e36621ebe
Jake Samuels <samuels242@googlemail.com>
2023-07-21 15:06:11 +0100

    Add getJobById function

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,35 +5,7 @@ const devjobsApi = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1/",
 });
 
-// GET all jobs
-export const getAllJobs = async ({ search, location, contract }) => {
-  try {
-    // Get the token from localStorage
-    const token = localStorage.getItem("token");
-
-    // Define custom headers for this API call
-    const headers = {
-      Authorization: `Bearer ${token}`,
-      // Add other headers here if needed
-    };
-
-    let url;
-
-    url =
-      search || location || contract
-        ? `/jobs?search=${search}&location=${location}&contract=${contract}`
-        : "/jobs";
-
-    const response = await devjobsApi.get(url, { headers });
-
-    const jobsData = response.data.data.jobs;
-
-    return jobsData;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
+// AUTHENTICATION
 
 // Log in user
 export const login = async (email, password) => {
@@ -82,6 +54,58 @@ export const signUp = async (name, email, password, passwordConfirm, role) => {
   } catch (error) {
     console.error("Sign Up failed:", error);
     // You can handle sign-up errors here or throw an error to be caught in the calling code.
+    throw error;
+  }
+};
+
+// JOBS
+
+// GET all jobs
+export const getAllJobs = async ({ search, location, contract }) => {
+  try {
+    // Get the token from localStorage
+    const token = localStorage.getItem("token");
+
+    // Define custom headers for this API call
+    const headers = {
+      Authorization: `Bearer ${token}`,
+      // Add other headers here if needed
+    };
+
+    let url;
+
+    url =
+      search || location || contract
+        ? `/jobs?search=${search}&location=${location}&contract=${contract}`
+        : "/jobs";
+
+    const response = await devjobsApi.get(url, { headers });
+
+    const jobsData = response.data.data.jobs;
+
+    return jobsData;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+// GET job by id
+export const getJobById = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    const url = `/jobs/${id}`;
+
+    const response = await devjobsApi.get(url, { headers });
+
+    return response.data.data.job;
+  } catch (error) {
+    console.error(error);
     throw error;
   }
 };
