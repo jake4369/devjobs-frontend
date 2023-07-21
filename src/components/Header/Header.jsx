3ff4803f2bb4f8dada2a9cf0e545fa22da919742
Jake Samuels <samuels242@googlemail.com>
2023-07-21 15:03:34 +0100

    Conditionally render search bar depending on url

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import logo from "./../../assets/logos/devjobs.png";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 import MobileSearchForm from "./Forms/MobileSearchForm";
 import DesktopSearchForm from "./Forms/DesktopSearchForm";
+import WebsiteCard from "../JobSpecifiationPage/WebsiteCard";
 
 const ModeSwitch = () => {
   return <div className="header__mode-switch__container">Mode</div>;
@@ -10,22 +11,15 @@ const ModeSwitch = () => {
 
 const Header = ({ setShowModal, setSearchObject }) => {
   const [isMobile, setIsMobile] = useState(false);
-  const [isJobsPage, setIsJobsPage] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 768);
     };
 
-    const checkJobsPage = () => {
-      setIsJobsPage(window.location.pathname.endsWith("/jobs"));
-    };
-
     // Add event listener to track window resize
     window.addEventListener("resize", handleResize);
-
-    // Check if URL ends with "/jobs" on component mount
-    checkJobsPage();
 
     // Initial check on component mount
     handleResize();
@@ -36,21 +30,29 @@ const Header = ({ setShowModal, setSearchObject }) => {
     };
   }, []);
 
+  // Check if the URL ends with "/jobs" or "/jobs/:id"
+  const isJobsPage =
+    location.pathname.endsWith("/jobs") ||
+    location.pathname.match(/\/jobs\/\d+$/);
+
   return (
     <header>
-      <img src={logo} alt="" className="header__logo" />
+      <img src="" alt="" className="header__logo" />
 
       <ModeSwitch />
 
-      {isJobsPage &&
-        (isMobile ? (
+      {isMobile ? (
+        // Render MobileSearchForm if it's the jobs page, else render null (hide it)
+        isJobsPage ? (
           <MobileSearchForm
             setShowModal={setShowModal}
             setSearchObject={setSearchObject}
           />
-        ) : (
-          <DesktopSearchForm setSearchObject={setSearchObject} />
-        ))}
+        ) : null
+      ) : // Render DesktopSearchForm if it's the jobs page, else render null (hide it)
+      isJobsPage ? (
+        <DesktopSearchForm setSearchObject={setSearchObject} />
+      ) : null}
     </header>
   );
 };
