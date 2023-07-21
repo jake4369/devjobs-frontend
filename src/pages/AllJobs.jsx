e8bbc46df6a0b0dcc9fbef603b683ddc008598ab
Jake Samuels <samuels242@googlemail.com>
2023-07-21 08:36:41 +0100

    Pass setSearchObject as props to modal component

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -1,14 +1,19 @@
 import FilterModal from "../components/AllJobsPage/FilterModal";
 import JobCard from "../components/AllJobsPage/JobCard";
 
-const AllJobs = ({ showModal, setShowModal, jobsData }) => {
+const AllJobs = ({ showModal, setShowModal, jobsData, setSearchObject }) => {
   const jobCards = jobsData.map((job) => {
     return <JobCard key={job._id} job={job} />;
   });
 
   return (
     <div className="all-jobs-page">
-      {showModal && <FilterModal setShowModal={setShowModal} />}
+      {showModal && (
+        <FilterModal
+          setShowModal={setShowModal}
+          setSearchObject={setSearchObject}
+        />
+      )}
       <section className="job-cards">{jobCards}</section>
     </div>
   );
