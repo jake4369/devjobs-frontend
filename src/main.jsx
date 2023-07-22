6f1f697a5bf0247dcc14a7276d5dca0c39b13204
Jake Samuels <samuels242@googlemail.com>
2023-07-22 10:21:32 +0100

    Import and use ScreenSizeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { FormProvider } from "./context/FormContext.jsx";
+import { ScreenSizeProvider } from "./context/IsMobile";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <FormProvider>
-        <App />
+        <ScreenSizeProvider>
+          <App />
+        </ScreenSizeProvider>
       </FormProvider>
     </BrowserRouter>
   </React.StrictMode>
